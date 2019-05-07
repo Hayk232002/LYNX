76b1a14a268049aa9df9057bb57a19e4607f0f48
Hayk232002 <hayknazaryan10@gmail.com>
2019-05-07 23:11:22 +0400

    cod

--- app/src/main/java/com/example/lynx/Login.java
+++ app/src/main/java/com/example/lynx/Login.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
     Button btn_creatacc_login;
+//    EditText et_
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
