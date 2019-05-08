bb8a2f449cacb04a05c11f6cfc99401b142e40a3
Hayk232002 <hayknazaryan10@gmail.com>
2019-05-08 18:09:47 +0400

    cod

--- app/src/main/java/com/example/lynx/ProfileSettings.java
+++ app/src/main/java/com/example/lynx/ProfileSettings.java
@@ -1,13 +1,120 @@
 package com.example.lynx;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileSettings extends AppCompatActivity {
+    private static final int SELECT_PICTURE = 1;
+
+    CircleImageView iv_photo_profset;
+
+
+    private String selectedImagePath;
+    private Uri selectedImageUri;
+    private Uri downloadUri;
+
+    private FirebaseAuth mAuth;
+    private StorageReference storageReference;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+
+        storageReference= FirebaseStorage.getInstance().getReference();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        iv_photo_profset = (CircleImageView) findViewById(R.id.iv_photo_profset);
+
+        iv_photo_profset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkandroidversion();
+            }
+        });
+    }
+
+    public void pickImage() {
+        CropImage.startPickImageActivity(this);
+    }
+
+    public void checkandroidversion(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 555);
+            }catch (Exception e){}
+        }
+        else {
+            pickImage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 555 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            pickImage();
+        }
+        else {
+            checkandroidversion();
+        }
+    }
+
+    private void croprequest(Uri imageUri) {
+        CropImage.activity(imageUri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setMultiTouchEnabled(true)
+                .start(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //RESULT FROM SELECTED IMAGE
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri imageUri = CropImage.getPickImageResultUri(this, data);
+            croprequest(imageUri);
+            selectedImageUri = imageUri;
+
+            Log.wtf("image",selectedImageUri.toString());
+        }
+
+        //RESULT FROM CROPING ACTIVITY
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), result.getUri());
+
+                    ((ImageView)findViewById(R.id.iv_photo_profset)).setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
